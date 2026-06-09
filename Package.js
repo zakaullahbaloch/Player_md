0bdd5c53b9d020943cf1f08b132a3fcbe6ffa979
Shahzu/Tech404 <168470039+zakaullahbaloch@users.noreply.github.com>
2026-06-09 09:32:05 +0500

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,18 @@
+{
+  "name": "player-md",
+  "version": "2.0.0",
+  "description": "PLAYER MD - WhatsApp Bot by SHAHZU PLAYER",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.5",
+    "qrcode-terminal": "^0.12.0",
+    "pino": "^8.17.2",
+    "fs-extra": "^11.2.0",
+    "axios": "^1.6.2"
+  },
+  "author": "SHAHZU PLAYER",
+  "license": "MIT"
+}
