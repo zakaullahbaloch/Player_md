655a44c40355405dd375802bbd76267eca0ae150
Shahzu/Tech404 <168470039+zakaullahbaloch@users.noreply.github.com>
2026-06-09 09:33:15 +0500

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PREFIX: '.',
+    OWNER_NUMBER: '923033065965',
+    OWNER_NAME: 'SHAHZU PLAYER',
+    BOT_NAME: 'PLAYER MD',
+    ALWAYS_ONLINE: true,
+    AUTO_STATUS_VIEW: true,
+    TIMEZONE: 'Asia/Karachi'
+};
